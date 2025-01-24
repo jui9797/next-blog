f259b2d27e8f6e1b235a7eab0b8cd9457646e966
Lenovo <jannatjui414@gmail.com>
2025-01-24 12:45:26 +0600

    Some updated

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,8 @@ import Blogs from "@/components/Blogs";
 const page = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts')
   const blogs = await res.json();
-  // console.log(blogs);
   return (
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7">
       {
         blogs?.map(blog => <Blogs key={blog.id} blog={blog}></Blogs>)
       }
