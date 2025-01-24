59e703de0700fc6eb0f37a38b3f137d4deea7670
Lenovo <jannatjui414@gmail.com>
2025-01-24 14:38:35 +0600

    make some changes

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,8 +1,11 @@
 
-const page = () => {
+const page = async() => {
+    const { getUser } = getKindeServerSession();
+        const user = await getUser();
+        console.log(user)
     return (
         <div>
-            <h2 className="text-3xl font-bold">Hi, Welcome to your porfile</h2>
+            <h2 className="my-4 text-3xl font-bold text-center">Hi, Welcome to your porfile</h2>
         </div>
     );
 };
