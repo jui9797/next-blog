dd1e4d3c56ecf7c6760c2c88623f90b48ba1d432
Lenovo <jannatjui414@gmail.com>
2025-01-24 12:56:13 +0600

    fixing navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,38 +14,18 @@ const Navbar = async () => {
         <div>
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h8m-8 6h16" />
-                            </svg>
-                        </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow space-x-4">
-                            <Link href='/'>Home</Link>
-                            <Link href='/profile'>Profile</Link>
-                        </ul>
-                    </div>
                     <a className="text-xl">Blog-Viewer</a>
                 </div>
-                
+
                 <div className="navbar-end">
+                    <Link href='/' className='text-black'>Home</Link>
+                    <Link href={`${user ? '/profile' : '/api/auth/login'}`} className='text-black'>Profile</Link>
                     {
                         user ? <>
-                            <button className='btn btn-primary'><LogoutLink>Log out</LogoutLink></button>
+                            <LogoutLink> <button className='btn btn-primary'>Log out</button></LogoutLink>
                         </> : <>
-                            <button className='btn btn-primary'><LoginLink>Sign in</LoginLink></button>
-                            <button className='btn btn-primary ml-2'><RegisterLink>Sign up</RegisterLink></button>
+                            <LoginLink><button className='btn btn-primary'>Sign in</button></LoginLink>
+                            <RegisterLink> <button className='btn btn-primary ml-2'>Sign up</button></RegisterLink>
                         </>
                     }
                 </div>
